25d3687951cf3d6785b604b062de73bf977be2e8
avisser-98 <avisser@ucsc.edu>
2026-01-11 21:12:47 -0800

    Adding Code Journal 1 Prompt 2

---
+++ journal1sesh2.py
@@ -0,0 +1,15 @@
+float1 = float(6.9067)
+float2 = float(4.2066)
+sum = float1 + float2
+print(sum)
+print(type(sum))
+
+integer1 = int(54)
+integer2 = int(31)
+diff = integer1 - integer2
+print(diff)
+print(type(diff))
+
+product = float1 + integer2
+print(product)
+print(type(product))
