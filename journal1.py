5be680cba2f09bf6da5cbb62e163cc4b9cf59d33
avisser-98 <avisser@ucsc.edu>
2026-01-11 20:47:04 -0800

    Adding Code Journal 1 Prompt 1

---
+++ journal1.py
@@ -0,0 +1,4 @@
+print("My name is Aiden Visser")
+print("My pronouns are He/They")
+print("My favorite movie is The Big Lebowski. I watched it with my boyfriend and we both love it.")
+print("My favorite food is pancakes. The pancakes from IHOP are my favorite. :)")
